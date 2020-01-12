693b0cdb5dd6eca4d1327124914390d5fef7f980
jasylwong <jasylwong@gmail.com>
2020-01-12 15:03:00 +0000

    pass alt test 4

--- lib/fizzbuzz_alt.rb
+++ lib/fizzbuzz_alt.rb
@@ -1,5 +1,11 @@
 class Integer
     def fizzbuzz
-        'fizz'
+        if self == 15 
+            'fizzbuzz'
+        elsif self % 3 == 0
+            'fizz'
+        elsif self == 5
+            'buzz'       
+        end
     end
 end
--- spec/fizzbuzz_alt_spec.rb
+++ spec/fizzbuzz_alt_spec.rb
@@ -4,4 +4,16 @@ describe "fizzbuzz" do
     it 'returns "fizz" for the number 3' do
         expect(3.fizzbuzz).to eq 'fizz'
     end
+
+    it 'returns "buzz" for the number 5' do
+        expect(5.fizzbuzz).to eq 'buzz'
+    end
+
+    it 'returns "fizzbuzz" for the number 15' do
+        expect(15.fizzbuzz).to eq 'fizzbuzz'
+    end
+
+    it 'returns "fizz" for the number 9' do
+        expect(9.fizzbuzz).to eq 'fizz'
+    end
 end
