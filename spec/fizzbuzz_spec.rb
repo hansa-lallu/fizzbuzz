002b61af3d621852dbc7756b7ba66cd5b29c837f
hansa-lallu <hansalallu@gmail.com>
2020-01-12 14:01:16 +0000

    third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,6 +6,10 @@ describe 'fizzbuzz' do
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)). to eq 'buzz'
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
