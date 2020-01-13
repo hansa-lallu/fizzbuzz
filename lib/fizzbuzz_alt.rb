185b44b09804dab390cd344189d56cea502aa050
jasylwong <jasylwong@gmail.com>
2020-01-13 21:10:37 +0000

    Created a fizzbuzz method for the Integer class

--- lib/fizzbuzz_alt.rb
+++ lib/fizzbuzz_alt.rb
@@ -1,11 +1,17 @@
 class Integer
     def fizzbuzz
-        if self == 15 
+        if self < 0
+            # We are ignoring negative numbers for the sake of practicing more tests and edge cases,
+            # but you could still include them in a fizzbuzz method if desired. 
+            'error'
+        elsif self % 15 == 0 
             'fizzbuzz'
         elsif self % 3 == 0
             'fizz'
-        elsif self == 5
-            'buzz'       
+        elsif self % 5 == 0
+            'buzz' 
+        else
+            self      
         end
     end
 end
--- spec/fizzbuzz_alt_spec.rb
+++ spec/fizzbuzz_alt_spec.rb
@@ -16,4 +16,21 @@ describe "fizzbuzz" do
     it 'returns "fizz" for the number 9' do
         expect(9.fizzbuzz).to eq 'fizz'
     end
+
+    it 'returns "buzz" for the number 10' do
+        expect(10.fizzbuzz).to eq 'buzz'
+    end 
+
+    it 'returns "fizzbuzz" for the number 30' do
+        expect(30.fizzbuzz).to eq 'fizzbuzz'
+    end
+
+    it 'returns "2" for the number 2' do
+        expect(2.fizzbuzz).to eq 2
+    end
+
+    it 'returns "error" for -3' do
+        expect(-3.fizzbuzz).to eq 'error'
+    end
+
 end
